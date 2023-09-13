d3cdfb82d792d1ce32fc2cce94bec168d4e958dd
Pauliin7 <pauloacs2007@gmail.com>
2023-09-13 16:53:50 -0300

    menu principal

---
+++ java.js
@@ -0,0 +1,30 @@
+// array para adicionar os produtos
+var lista_de_produtos = []
+
+while(true){
+
+var opcao = prompt (`
+Bem vindo ao cadastro de produtos ! :)
+
+1. Adicionar produtos
+2. Editar produtos
+3. Remover produtos
+4. Visualizar todos os produtos
+`)
+
+if (opcao== 1) {
+    //codigo para adicionar o produto
+}else if (opcao == 2){
+    //codigo para editar o produto
+}else if (opcao == 3){
+    //codigo para remover produto
+}else if (opcao == 4){
+    // codigo para visualizar os produtos
+}else{
+    alert("Nenhuma das opções foram selecionadas")
+}
+
+
+
+}
+
